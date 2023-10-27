4437c652292e72c436160fffb43c6173fb0329c1
miguelsndc <mglsndc@gmail.com>
2023-10-27 20:44:52 -0300

    fix dap python

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("luasnip.loaders.from_vscode").lazy_load()
+require("dap-python").setup("C:/Users/Paula/.virtualenvs/debugpy/Scripts/python")
+require("nvim-treesitter.configs").setup({
+  autotag = {
+    enable = true,
+    enable_rename = true,
+  },
+})
