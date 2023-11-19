1ef65d4dd6c065c9acadd4bad1f88e1e38def198
miguelsndc <mglsndc@gmail.com>
2023-11-19 01:34:23 -0300

    add compiler.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,6 @@
 
 vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
 vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+
+vim.keymap.set("n", "<F6>", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F7>", "<cmd>CompilerRedo<cr>", { noremap = true, silent = true })
